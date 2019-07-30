01d603178b98e1666ce55cdf3cbe0ef5f2abc4aa
이상민 <sm0514sm@gmail.com>
2019-07-30 09:33:38 +0900

    190729 SW Expert 1218

--- src/SW_Expert/_1218_괄호짝짓기.java
+++ src/SW_Expert/_1218_괄호짝짓기.java
@@ -1,26 +1,31 @@
 package SW_Expert;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class _1218_괄호짝짓기 {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		System.setIn(new FileInputStream("1218.txt"));
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.setIn(new FileInputStream("rsc/_SW_Expert_1218.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for (int test_case = 1; test_case <= 10; test_case++) {
 			int answer = 1;
 			Stack<Character> stack = new Stack<Character>();
-			int num = sc.nextInt();
-			sc.nextLine();
-			String text = sc.nextLine();
+			int num = Integer.parseInt(br.readLine());
+			String text = br.readLine();
+			
 			FOR_LOOP:
 			for(int i = 0; i < text.length(); i++) {
 				if(text.charAt(i) == '<' || text.charAt(i) == '(' || text.charAt(i) == '[' || text.charAt(i) == '{')
 					stack.push(text.charAt(i));
 				else {
+					if(stack.isEmpty()) {
+						answer = 0;
+						break;
+					}
 					switch (text.charAt(i)) {
 					case '>':
 						if(stack.pop() != '<') {
@@ -49,16 +54,9 @@ public class _1218_괄호짝짓기 {
 					}
 				}
 			}
-			
-			
-			
 			System.out.printf("#%d %d\n", test_case, answer);
 		}
 		
-		
-		sc.close();
-		
-		
+		br.close();
 	}
-
 }
