472b89e4096de5a83f20b1a5f095890af4f88a37
이상민 <sm0514sm@gmail.com>
2019-07-31 09:05:54 +0900

    정올 1809 탑

--- src/jungol/_1809_탑.java
+++ src/jungol/_1809_탑.java
@@ -1,32 +1,39 @@
 package jungol;
 
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class _1809_탑 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		Stack<Integer> stack = new Stack<Integer>();
-		Stack<Integer> tempS;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());	
+		String[] temp = br.readLine().split("\\s");
+		int[] top  = new int[N];	// 타워의 높이
+		int[] answer = new int[N];	// answer[i] = i번째 타워 레이저 수신 타워
+		
 		for (int i = 0; i < N; i++) {
-			boolean isPrint = false;
-			tempS = (Stack<Integer>) stack.clone();
-			int next = sc.nextInt();
-			stack.push(next);
-			while (tempS.size() != 0) {
-				int temp = tempS.pop();
-				if (temp > next) {
-					System.out.print((tempS.size() + 1) + " ");
-					isPrint = true;
+			top[i] = Integer.parseInt(temp[i]);	// 하나씩 입력받기
+			for (int j = i-1; j >= 0; j--) {	// 현재보는 타워의 왼쪽부터 차례대로
+				if(top[j] > top[i]) {			// 왼쪽의 타워가 더 높은 경우
+					answer[i] = j+1;
 					break;
 				}
+				else {
+					if(answer[j] != 0)			// 왼쪽의 타워가 높지 않지만 수신 타워가 있다면
+						j = answer[j];			// 비교할 타워를 위의 수신 타워로
+					else {						// 왼쪽의 타워가 더 낮은데도 수신 타워가 없다면 현재 타워도 수신 타워 없음
+						answer[i] = 0;
+						break;
+					}
+				}
 			}
-			if (!isPrint)
-				System.out.print(0 + " ");
-
+			bw.write(answer[i] + " ");
 		}
-
-		sc.close();
+		bw.flush();
+		bw.close();
 	}
 }
