9eebb76cc16285137bbb4d09b9d5fc6f149a3547
이상민 <sm0514sm@gmail.com>
2019-08-06 09:43:13 +0900

    190805 수정

--- src/_SW_Expert/_1225_암호생성기.java
+++ src/_SW_Expert/_1225_암호생성기.java
@@ -15,24 +15,23 @@ public class _1225_암호생성기 {
 		for(int i = 0; i < 10; i ++) {
 			queue.clear();
 			sc.nextInt();
-			for(int j = 0; j < 8; j++)
-				queue.offer(sc.nextInt());
-			
-			while(true) {
-				for(int j = 1; j <= 5; j++) {
+			for(int j = 0; j < 8; j++)	queue.offer(sc.nextInt());
+
+			while (true) {
+				for (int j = 1; j <= 5; j++) {
 					int temp = queue.poll() - j;
-					if(temp <= 0) {
+					if (temp <= 0) {
 						temp = 0;
 						queue.offer(temp);
-						System.out.print("#" + (i+1) + " ");
-						for(int k = 0; k < 8; k++)
+						System.out.print("#" + (i + 1) + " ");
+						for (int k = 0; k < 8; k++)
 							System.out.print(queue.poll() + " ");
 						System.out.println();
 						continue next_case;
 					}
 					queue.offer(temp);
 				}
-			}
+			} // END while
 		}
 		sc.close();
 	}
--- src/workshop_0805/HW1_마이쮸_이상민.java
+++ src/workshop_0805/HW1_마이쮸_이상민.java
@@ -3,11 +3,12 @@ package workshop_0805;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class myZZu {
+public class HW1_마이쮸_이상민 {
 	
 	public static void main(String[] args) {
 		Queue<Integer> queue = new LinkedList<Integer>();
-		int[] count = new int[10];
+		int[] count = new int[20];		
+		// 마이쮸 받을 사람 수를 만들어야하는데 큐에 이거까지 집어넣으면 효율성 증가
 		int MAX_ZZU = 20;
 		int who = 1;
 		int pop = 0;
