9d62bdb435ae4df4ae26e133a120d87510ae6801
이상민 <sm0514sm@gmail.com>
2019-08-12 17:13:27 +0900

    190806 수정

--- src/workshop_0806/MergeSort.java
+++ src/workshop_0806/MergeSort.java
@@ -6,10 +6,14 @@ public class MergeSort {
 
 	public static void main(String[] args) {
 		int[] arr = { 69, 10, 30, 2, 16, 8, 31, 22, 100, 1 };
-		System.out.println(Arrays.toString(mergeSort(arr, 0, arr.length)));
+		System.out.println("	원본 : " + Arrays.toString(arr));
+		System.out.println("상민	구현 : " + Arrays.toString(mergeSortSM(arr, 0, arr.length))); // 상민 구현
+		System.out.println("	원본 : " + Arrays.toString(arr));
+		mergeSortTC(arr, 0, arr.length - 1);
+		System.out.println("  쌤	구현 : " + Arrays.toString(arr));
 	}
-
-	private static int[] mergeSort(int[] arr, int L, int R) {
+	/*------------- 상민 구현 ---------------------------*/
+	private static int[] mergeSortSM(int[] arr, int L, int R) {
 		if(arr.length == 1) return arr;
 		
 		R -= L;	L -= L;
@@ -18,10 +22,10 @@ public class MergeSort {
 		int[] left = Arrays.copyOfRange(arr, L, mid);
 		int[] right = Arrays.copyOfRange(arr, mid, R);
 
-		return merge(mergeSort(left, 0, mid), mergeSort(right, mid, R));
+		return mergeSM(mergeSortSM(left, 0, mid), mergeSortSM(right, mid, R));
 	}
 
-	private static int[] merge(int[] left, int[] right) {
+	private static int[] mergeSM(int[] left, int[] right) {
 		int[] merged = new int[left.length + right.length];
 		int pointL = 0;
 		int pointR = 0;
@@ -40,6 +44,38 @@ public class MergeSort {
 		return merged;
 	}
 
-	
-	
+	/*------------- 쌤 구현 ---------------------------*/
+	public static void mergeSortTC(int[] list, int start, int end) {
+		if (start == end)
+			return;
+		// 현집합을 반으로 나누어 정렬
+		int half = (start + end) / 2;
+
+		// 왼쪽 집합 정렬해오기
+		mergeSortTC(list, start, half);
+		// 오른쪽집합 정렬해오기
+		mergeSortTC(list, half + 1, end);
+
+		// 정렬된 두 집합 이용하여 합치기
+		mergeTC(list, start, half, end);
+	}
+
+	private static void mergeTC(int[] list, int start, int half, int end) {
+		int newArr[] = new int[end - start + 1];
+		int left = start, right = half + 1;
+		int i = 0;
+
+		do {
+			if (list[left] < list[right]) {
+				newArr[i++] = list[left++];
+			} else
+				newArr[i++] = list[right++];
+		} while (left <= half && right <= end);
+		
+		while(left<=half) newArr[i++] = list[left++];	//   왼쪽 집합이 남은 경우
+		while(right<=end) newArr[i++] = list[right++];	// 오른쪽 집합이 남은 경우
+		
+		System.arraycopy(newArr, 0, list, start, newArr.length);
+	}
+
 }
