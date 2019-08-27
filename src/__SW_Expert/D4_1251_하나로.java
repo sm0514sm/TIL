b6538f2a962bac74466447edc25597b6d1c8f705
leesangmin <sm0514sm@gmail.com>
2019-08-27 09:08:44 +0900

    190826 HW WS

--- src/__SW_Expert/D4_1251_하나로.java
+++ src/__SW_Expert/D4_1251_하나로.java
@@ -8,40 +8,36 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class D4_1251_하나로 {
-	public static class ISLAND implements Comparable<ISLAND> {
+	public static class ISLAND {
 		int x, y, num;
-
-		public ISLAND() {}
-
-		@Override
-		public int compareTo(ISLAND o) {
-			return 0;
-		}
 	}
-	
-	public static class EDGE implements Comparable<EDGE>{
+
+	public static class EDGE implements Comparable<EDGE> {
 		ISLAND left, right;
 		double len;
+
 		public EDGE(ISLAND left, ISLAND right) {
-			super();
 			this.left = left;
 			this.right = right;
 			this.len = getDistance(this.left, this.right);
 		}
-		
+
 		@Override
 		public int compareTo(EDGE o) {
-			if(this.len < o.len)			return -1;
-			else if(this.len > o.len)		return 1;
-			else							return 0;
+			if (this.len < o.len)
+				return -1;
+			else if (this.len > o.len)
+				return 1;
+			else
+				return 0;
 		}
 	}
-	
+
 	static int[] arr = new int[2];
 	static int N, cnt, parents[];
 	static ISLAND[] islands;
 	static EDGE[] edges;
-	
+
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("rsc/_SW_Expert_1251.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -50,7 +46,7 @@ public class D4_1251_하나로 {
 			double answer = 0;
 			N = Integer.parseInt(br.readLine());
 			islands = new ISLAND[N];
-			edges = new EDGE[N*(N-1)/2];
+			edges = new EDGE[N * (N - 1) / 2];
 			cnt = 0;
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
@@ -63,51 +59,55 @@ public class D4_1251_하나로 {
 				islands[j].num = j;
 			}
 			double e = Double.parseDouble(br.readLine());
-			combination(0,0);
-			Arrays.sort(edges);
+			/* 데이터 입력 완료 */
 			
+			combination(0, 0);
+			Arrays.sort(edges);
+
 			parents = new int[N];
-            Arrays.fill(parents, -1);
-            int edgeCnt = 0;
-            int select = -1;
-            while (edgeCnt != N - 1) {
-                select++;
-                if (union(edges[select].left.num, edges[select].right.num)) {
-                    edgeCnt++;
-                    answer += Math.pow(edges[select].len,2);
-                }
-            }
-            long ans = Math.round(answer * e);
-            System.out.println("#" + test_case +" " + ans);
+			Arrays.fill(parents, -1);
+			int edgeCnt = 0;
+			int select = -1;
+			while (edgeCnt != N - 1) {
+				select++;
+				if (union(edges[select].left.num, edges[select].right.num)) {
+					edgeCnt++;
+					answer += Math.pow(edges[select].len, 2);
+				}
+			}
+			long ans = Math.round(answer * e);
+			System.out.println("#" + test_case + " " + ans);
 		}
 	}
+
 	private static void combination(int before, int index) {
-		if(index == 2) {
+		if (index == 2) {
 			edges[cnt++] = new EDGE(islands[arr[0]], islands[arr[1]]);
 			return;
 		}
-		for(int i = before; i < N; i++) {
+		for (int i = before; i < N; i++) {
 			arr[index] = i;
-			combination(i + 1, index+1);
+			combination(i + 1, index + 1);
 		}
 	}
+
 	private static double getDistance(ISLAND island, ISLAND island2) {
-		return Math.sqrt(Math.pow((island.x - island2.x), 2)+Math.pow((island.y - island2.y), 2));
+		return Math.sqrt(Math.pow((island.x - island2.x), 2) + Math.pow((island.y - island2.y), 2));
 	}
-	
+
 	private static boolean union(int a, int b) {
-        int aRoot = find(a);
-        int bRoot = find(b);
-        if (aRoot != bRoot) {
-            parents[bRoot] = aRoot;
-            return true;
-        }
-        return false;
-    }
- 
-    private static int find(int a) {
-        if (parents[a] < 0)
-            return a;
-        return parents[a] = find(parents[a]); // Path Compression
-    }
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if (aRoot != bRoot) {
+			parents[bRoot] = aRoot;
+			return true;
+		}
+		return false;
+	}
+
+	private static int find(int a) {
+		if (parents[a] < 0)
+			return a;
+		return parents[a] = find(parents[a]); // Path Compression
+	}
 }
