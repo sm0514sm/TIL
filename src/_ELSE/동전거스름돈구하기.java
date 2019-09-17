3b3c01df35853325e399c8b17cfba4bc8bc490b4
sangminLee <sm0514sm@gmail.com>
2019-09-17 17:12:02 +0900

    190917

--- src/_ELSE/동전거스름돈구하기.java
+++ src/_ELSE/동전거스름돈구하기.java
@@ -8,14 +8,14 @@ public class 동전거스름돈구하기 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int Coin[] = {1, 4, 6};
-		int Memo[] = new int[N+1];
+		int Coin[] = { 1, 4, 6 };
+		int Memo[] = new int[N + 1];
 		Arrays.fill(Memo, Integer.MAX_VALUE);
 		Memo[0] = 0;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 1; j <= N; j++) {
-				if(j - Coin[i] >= 0)
-					Memo[j] = Math.min(Memo[j-Coin[i]] + 1, Memo[j]);
+		for (int j = 1; j <= N; j++) {
+			for (int i = 0; i < 3; i++) {
+				if (j - Coin[i] >= 0)
+					Memo[j] = Math.min(Memo[j - Coin[i]] + 1, Memo[j]);
 			}
 		}
 		System.out.println(Memo[N]);
