0612a5188d8a33b024443fbdd0949aecde5847a7
lifaon <sm0514sm@gmail.com>
2019-10-14 09:17:01 +0900

    .

--- src/__Backjoon/_01786_찾기.java
+++ src/__Backjoon/_01786_찾기.java
@@ -17,8 +17,8 @@ public class _01786_찾기 {
 		int tLength = T.length, pLength = P.length;
 		int[] fail = new int[pLength];
 
-//		 실패함수 만들기 : P(패턴)에서 다시 P를 찾는 형태로
-//		 해당 부분 문자열의 (접두사와 일치하는) 최대 접미사의 길이
+//		실패함수 만들기 : P(패턴)에서 다시 P를 찾는 형태로
+//		해당 부분 문자열의 (접두사와 일치하는) 최대 접미사의 길이
 		for (int i = 1, j = 0; i < pLength; i++) { // i : 접미사 포인터, j : 접두사포인터
 			
 			// j위치에서 틀리면 j-1위치까지는 맞았다는 이야기
@@ -28,8 +28,8 @@ public class _01786_찾기 {
 			if (P[i] == P[j]) fail[i] = ++j;
 		}
 		int cnt = 0;
-//		 i: 텍스트 포인터, j: 패턴포인터
-//		 i는 계속해서 증가함
+//		i: 텍스트 포인터, j: 패턴포인터
+//		i는 계속해서 증가함
 		for (int i = 0, j = 0; i < tLength; i++) {
 			while (j > 0 && T[i] != P[j]) j = fail[j - 1];
 			if (T[i] == P[j]) {
