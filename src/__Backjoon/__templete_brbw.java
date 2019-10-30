f9b2b914734f840138f93b7e7731c70bb3886b26
lifaon <sm0514sm@gmail.com>
2019-10-30 19:47:11 +0900

    .

--- src/__Backjoon/__templete_brbw.java
+++ src/__Backjoon/__templete_brbw.java
@@ -1,20 +1,15 @@
 package __Backjoon;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 //Main
 public class __templete_brbw {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 //		StringTokenizer st = null;
 		int answer = 0;
 //		st = new StringTokenizer(br.readLine());
 		
-		bw.write(String.format("%d \n", answer));
-		bw.flush();
 	}
 }
