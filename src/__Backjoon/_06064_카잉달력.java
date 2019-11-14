903b1b9b3daa564927d557a7e8247cd6230166ee
lifaon <sm0514sm@gmail.com>
2019-11-14 18:05:10 +0900

    651651

---
+++ src/__Backjoon/_06064_카잉달력.java
@@ -0,0 +1,23 @@
+package __Backjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//Main
+public class _06064_카잉달력 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 0; tc < T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			a*M + x = b*N + y
+		}
+	}
+}
