4a6e7782b6dfb29cba930949a5737546dc71d600
sm0514sm <sm0514sm@ajou.ac.kr>
2020-12-20 20:42:46 +0900

    2. View의 개념과 Layout

---
+++ 1_Android_Part_1(UI_Programming)/Workspace/2_2_LinearLayout/app/src/main/java/com/woobok/a2_2_linearlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.woobok.a2_2_linearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
