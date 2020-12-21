53eefdecf7de7ffe1be6c3114cc4b8c68d5297cd
sm0514sm <sm0514sm@ajou.ac.kr>
2020-12-22 00:45:27 +0900

    1 - 2. View의 개념과 Layout

---
+++ 1_Android_Part_1(UI_Programming)/Workspace/2_7_Space/app/src/main/java/com/woobok/a2_7_space/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.woobok.a2_7_space
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ 1_Android_Part_1(UI_Programming)/Workspace/2_9_IncludeOtherLayout/app/src/main/java/com/woobok/a2_9_includeotherlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.woobok.a2_9_includeotherlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
