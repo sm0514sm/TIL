a816251a9d3f7282e266ecae31bbf2f6b0e9756a
sm0514sm <sm0514sm@ajou.ac.kr>
2021-01-03 21:32:29 +0900

    1 - 3. Widget의 개념과 활용 (ProgressBar)

--- 1_Android_Part_1(UI_Programming)/Workspace/3_12_ProgressBar/app/src/main/java/com/woobok/a3_12_progressbar/MainActivity.kt
+++ 1_Android_Part_1(UI_Programming)/Workspace/3_12_ProgressBar/app/src/main/java/com/woobok/a3_12_progressbar/MainActivity.kt
@@ -2,10 +2,26 @@ package com.woobok.a3_12_progressbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView.text = "현재 값: ${progressBar4.progress}"
+        }
+
+        button2.setOnClickListener {
+            progressBar4.progress = 150
+        }
+
+        button3.setOnClickListener {
+            progressBar4.incrementProgressBy(10)
+        }
+        button4.setOnClickListener {
+            progressBar4.incrementProgressBy(-10)
+        }
     }
 }
