3af7b1f35440edb8e66237d048db1e68330a76ca
sm0514sm <sm0514sm@ajou.ac.kr>
2021-01-05 22:15:52 +0900

    1 - 3. Widget의 개념과 활용 (CardView)

---
+++ 1_Android_Part_1(UI_Programming)/Workspace/3_16_CardView/app/src/main/java/com/woobok/a3_16_cardview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.woobok.a3_16_cardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
