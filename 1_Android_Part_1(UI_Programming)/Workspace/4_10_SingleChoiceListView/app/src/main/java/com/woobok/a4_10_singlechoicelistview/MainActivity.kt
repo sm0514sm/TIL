a0dd0c03e52a1d295ae10158d8a439b88fd6399b
sm0514sm <sm0514sm@ajou.ac.kr>
2021-01-17 22:58:17 +0900

    1 - 4. AdapterView (SingleChoiceListView)

---
+++ 1_Android_Part_1(UI_Programming)/Workspace/4_10_SingleChoiceListView/app/src/main/java/com/woobok/a4_10_singlechoicelistview/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.woobok.a4_10_singlechoicelistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var data1 = arrayOf("항목1", "항목2", "항목3", "항목4", "항목5", "항목6", "항목7", "항목8")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter1 = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, data1)
+        list1.adapter = adapter1
+        list1.choiceMode = ListView.CHOICE_MODE_SINGLE
+        list1.setItemChecked(2, true)
+
+        button.setOnClickListener {
+            textView.text = "${data1[list1.checkedItemPosition]} 항목이 선택되어 있음"
+        }
+
+        list1.setOnItemClickListener { adapterView, view, i, l ->
+            textView.text = "${data1[list1.checkedItemPosition]} 항목이 선택되어 있음"
+        }
+    }
+}
