ee2dfa8e82b09c8483c5bdd50582f707f2fc73d9
sangminLee <sm0514sm@ajou.ac.kr>
2021-02-14 22:35:05 +0900

    Add Algorithm 06 - "06 가장 긴 팰린드롬 부분 문자열"

--- Algorithm/Python Algorithm Interview/06. 문자열 조작/06 가장 긴 팰린드롬 부분 문자열.py
+++ Algorithm/Python Algorithm Interview/06. 문자열 조작/06 가장 긴 팰린드롬 부분 문자열.py
@@ -1,27 +1,26 @@
 # 2021-01-26
 # 리트코드 5. Longest Palindrome Substring
+# https://leetcode.com/problems/longest-palindromic-substring
+# Q: 가장 긴 팰린드롬 부분 문자열 찾아내라
+def longest_palindrome(s: str) -> str:
+    # left ~ right까지 팰린드롬인지 확인 후 확장
+    def expand(left, right) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1: right]
 
-# Q: 애너그램 단위로 그룹핑하라  You can return the answer in any order.
-def my_solution(text: str) -> str:
-    max_num, max_key = 0, text[0]
-    for i in range(len(text)):
-        cnt = 0
-        while i - cnt >= 0 and i + cnt < len(text) and text[i-cnt] == text[i+cnt]:
-            cnt += 1
-        if max_num < (cnt-1)*2 + 1:
-            max_num = (cnt-1) * 2 + 1
-            max_key = text[i-cnt+1:i+cnt]
+    # 예외 처리
+    if len(s) < 2 or s == s[::-1]:
+        return s
 
-        cnt = 0
-        max_num = 0
-        while i - cnt >= 0 and i + cnt + 1 < len(text) and text[i-cnt] == text[i+cnt + 1]:
-            cnt += 1
-        if max_num < cnt*2:
-            max_num = cnt*2
-            max_key = text[i-cnt+1:i+cnt+1]
-    return max_key
+    result = ''
+    for i in range(len(s) - 1):
+        # 짝수, 홀수일 때 각각 팰린드롬 판단
+        result = max(result, expand(i, i + 1), expand(i, i + 2), key=len)
+    return result
 
 
-# print(my_solution("babad"))
-# print(my_solution("cbbd"))
-print(my_solution("bb"))
+print(longest_palindrome("babad"))
+print(longest_palindrome("cbbd"))
+print(longest_palindrome("bb"))
