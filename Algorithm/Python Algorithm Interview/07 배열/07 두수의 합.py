3c1462f6b257865cdfde015aea2e35da96cae789
sangminLee <sm0514sm@ajou.ac.kr>
2021-02-14 23:08:57 +0900

    Add Algorithm 07 - "07 두 수의 합"

---
+++ Algorithm/Python Algorithm Interview/07 배열/07 두수의 합.py
@@ -0,0 +1,21 @@
+# 2021-02-14
+# https://leetcode.com/problems/two-sum/
+# 리스트에서 타겟을 만들 수 있는 두 숫자 인덱스 리턴
+def solution(nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums)):
+        try:
+            return [i, nums.index(target - nums[i], i + 1)]
+        except ValueError:
+            continue
+
+
+def solution2(nums: list[int], target: int) -> list[int]:
+    nums_map = {}
+    for i, num in enumerate(nums):
+        if target - num in nums_map:
+            return [nums_map[target - num], i]
+        nums_map[num] = i
+
+
+# print(solution([2, 7, 11, 15], 9))
+print(solution2([0, 4, 3, 0], 0))
