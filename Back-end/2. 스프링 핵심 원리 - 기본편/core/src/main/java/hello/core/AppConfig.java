88dca90cda5b715c9b40cb0e2219e0e5118f8f8e
sangminLee <sm0514sm@ajou.ac.kr>
2021-12-19 23:03:54 +0900

    Add: 스프링 핵심 원리 - 기본편: 5. 싱글톤 컨테이너 - @Configuration과 싱글톤

--- Back-end/2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ Back-end/2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -15,16 +16,19 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public MemoryMemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
