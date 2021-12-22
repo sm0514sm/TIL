67734c9d8332a2062b3aa287e6a257fa21ea7b2d
sangminLee <sm0514sm@ajou.ac.kr>
2021-12-22 22:29:28 +0900

    Add: 스프링 핵심 원리 - 기본편: 6. 싱글톤 컨테이너 - 필터 & 중복 등록과 충돌

--- Back-end/2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AutoAppConfig.java
+++ Back-end/2. 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,14 +1,21 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         // @ComponentScan만 있으면 모든 @Component 가져오는데 @Configuration에도 이게 있어서 AppConfig와의 충돌 방지
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
--- Back-end/2. 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ Back-end/2. 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,7 +1,9 @@
 package hello.core.scan;
 
 import hello.core.AutoAppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
