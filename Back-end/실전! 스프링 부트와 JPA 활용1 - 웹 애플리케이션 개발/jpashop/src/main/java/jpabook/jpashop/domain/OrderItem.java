872d6c405e49e146bc44bcf20d81567ad89d6f81
sangminLee <sm0514sm@ajou.ac.kr>
2022-03-21 22:46:18 +0900

    Add: 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발: 6. 주문 도메인 개발 - Order, OrderItem 엔티티 개발

--- Back-end/실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ Back-end/실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@Table(name = "order_item")  // 에러나면 삭제하셈
 public class OrderItem {
     @Id
     @GeneratedValue
@@ -25,4 +26,31 @@ public class OrderItem {
 
     private int orderPrice; // 주문 당시 가격
     private int count; // 주문 당시 수량
+
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        // item.price와 다른 orderPrice로 구매할 경우(쿠폰 등) 대비
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //== 비즈니스 로직 ==//
+    public void cancel() {
+        // 아이템에 주문 당시 수량를 원복해줌
+        getItem().addStock(count);
+    }
+
+    //== 조회 로직 ==//
+
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
