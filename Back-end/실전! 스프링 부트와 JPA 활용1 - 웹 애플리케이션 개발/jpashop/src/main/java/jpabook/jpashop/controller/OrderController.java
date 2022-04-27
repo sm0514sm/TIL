f24e52ba6e8295acd068bdc134e2ebfd8ef1f7ea
sangminLee <sm0514sm@ajou.ac.kr>
2022-04-27 21:41:09 +0900

    Add: 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발: 7. 웹 계층 개발 - 상품 주문

---
+++ Back-end/실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        // RequestParam으로는 HTML에서 <select>의 name으로 지정한 값을 가져옴
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
