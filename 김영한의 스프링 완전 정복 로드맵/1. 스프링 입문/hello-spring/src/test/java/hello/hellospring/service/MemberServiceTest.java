66b58940f641bf0b21eab04fc68ab595bc099b8c
이상민 <211659@ssg.com>
2023-06-19 17:03:09 +0900

    스프링 입문: 3. 회원 관리 예제 - 백엔드 개발

---
+++ 김영한의 스프링 완전 정복 로드맵/1. 스프링 입문/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package hello.hellospring.service;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+class MemberServiceTest {
+
+  MemberService memberService;
+  MemoryMemberRepository repository;
+
+  @BeforeEach
+  public void beforeEach() {
+    repository = new MemoryMemberRepository();
+    memberService = new MemberService(repository);
+  }
+
+  @AfterEach
+  public void afterEach() {
+    repository.clearStore();
+  }
+
+  @Test
+  @DisplayName("회원가입")
+  void join() {
+    // given
+    Member member = new Member();
+    member.setName("hello");
+
+    // when
+    Long saveId = memberService.join(member);
+
+    // then
+    Optional<Member> findMember = memberService.findOne(saveId);
+    assertThat(member.getName()).isEqualTo(findMember.map(Member::getName).orElse(null));
+  }
+
+  @Test
+  @DisplayName("중복 회원 예외")
+  public void duplicateMember() {
+    // given
+    Member member1 = new Member();
+    member1.setName("hello");
+    Member member2 = new Member();
+    member2.setName("hello");
+
+    // when
+    memberService.join(member1);
+    IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+  }
+
+  @Test
+  void findMembers() {
+  }
+
+  @Test
+  void findOne() {
+  }
+}
