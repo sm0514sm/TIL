6ae307dc5a5b7d3e3b3f4bffa83bdde827e7e04e
이상민 <211659@ssg.com>
2023-06-30 14:52:28 +0900

    스프링 입문: 6. 스프링 DB 접근 기술 1

--- 김영한의 스프링 완전 정복 로드맵/1. 스프링 입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 김영한의 스프링 완전 정복 로드맵/1. 스프링 입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,22 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class SpringConfig {
+
+  private final DataSource dataSource;
 
   @Bean
   public MemberService memberService() {
@@ -17,6 +25,7 @@ public class SpringConfig {
 
   @Bean
   public MemberRepository memberRepository() {
-    return new MemoryMemberRepository();
+    // return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 }
