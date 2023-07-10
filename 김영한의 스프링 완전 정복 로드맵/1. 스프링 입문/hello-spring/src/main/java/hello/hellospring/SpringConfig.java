722c20123bbde85436891de1e94042363e7475d4
이상민 <211659@ssg.com>
2023-07-10 16:57:20 +0900

    스프링 입문: 7. AOP

--- 김영한의 스프링 완전 정복 로드맵/1. 스프링 입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ 김영한의 스프링 완전 정복 로드맵/1. 스프링 입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 
